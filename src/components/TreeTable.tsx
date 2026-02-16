cded44604d51d57a9d33740f8124b047347a689a
leropo <leropo@gmail.com>
2026-02-16 21:39:36 +0100

    moved collapse all button outside wrapper

--- src/components/TreeTable.tsx
+++ src/components/TreeTable.tsx
@@ -13,41 +13,41 @@ export default function TreeTable({ data }: {data: TableRow[]}) {
     };
 
     return (
-      <div className={showWithOverflow ? "table-wrapper-scrollable" : ""}>
-        <div className="table-controls">
-        </div>
+    <div>
+      <div className="table-controls">
+        <button 
+          className="global-toggle-button"
+          onClick={handleToggleAll}
+          aria-label={expandAll ? t.table.collapseAll : t.table.expandAll}
+        >
+          {expandAll ? `▼ ${t.table.collapseAll}` : `▶ ${t.table.expandAll}`}
+        </button>
+      </div>
 
-        <div className="table-controls">
-          <button 
-            className="global-toggle-button"
-            onClick={handleToggleAll}
-            aria-label={expandAll ? t.table.collapseAll : t.table.expandAll}
-          >
-            {expandAll ? `▼ ${t.table.collapseAll}` : `▶ ${t.table.expandAll}`}
-          </button>
+      <div className={showWithOverflow ? "table-wrapper-scrollable" : ""}>
+          <table className="data-table">
+            <thead>
+              <tr>
+                  <th>
+                  </th> 
+                  <th>
+                    {t.table.headers.outlineLevel}
+                  </th>  
+                  <th>
+                    {t.table.headers.code}
+                  </th> 
+                  <th>
+                    {t.table.headers.name}
+                  </th>
+                </tr>
+            </thead>
+            <tbody>
+              {data.map(node => (
+                <Row key={`data_row_${node.key}`} node={node} expandAll={expandAll} />
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table className="data-table">
-          <thead>
-            <tr>
-                <th>
-                </th> 
-                <th>
-                  {t.table.headers.outlineLevel}
-                </th>  
-                <th>
-                  {t.table.headers.code}
-                </th> 
-                <th>
-                  {t.table.headers.name}
-                </th>
-              </tr>
-          </thead>
-          <tbody>
-            {data.map(node => (
-              <Row key={`data_row_${node.key}`} node={node} expandAll={expandAll} />
-            ))}
-          </tbody>
-        </table>
       </div>
       );
   }
