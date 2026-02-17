d2033280c931c5ea9a04d1a66a074bfd4ab0b1cb
leropo <leropo@gmail.com>
2026-02-17 14:35:02 +0100

    generated separate utils for jsx creation and for parsing of data

---
+++ src/helpers/jsxUtils.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export function generateErrorListing(errorsData: string[]): React.ReactNode {
+    return (
+        <div style={{ overflowY: 'scroll', maxHeight:'400px' }}>
+          <table>
+            <thead>
+              <tr>
+                <th>Index</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {errorsData.map(({ index, value, errors }) => (
+                <React.Fragment key={index}>
+                  {/* Row with index + value */}
+                  <tr>
+                    <td>{index}</td>
+                    <td>{value}</td>
+                  </tr>
+      
+                  {/* Row with errors list spanning both columns */}
+                  <tr>
+                    <td colSpan={2}>
+                      <ul>
+                        {errors.map((err, i) => (
+                          <li key={i}>{err}</li>
+                        ))}
+                      </ul>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )
+}
+
+export function generateHeaderDifferences(current: string[], expected: string[]): React.ReactNode {
+    return (
+    <div style={{ overflowX: 'auto' }}>
+      <table style={{ borderCollapse: 'collapse' }}>
+        <tbody>
+          <tr>
+            <td style={{ fontWeight: 'bold', paddingRight: '1rem' }}>Provided</td>
+            {current.map((item, i) => (
+              <td key={i} style={{ padding: '0 8px' }}>{item}</td>
+            ))}
+          </tr>
+
+          <tr>
+            <td style={{ fontWeight: 'bold', paddingRight: '1rem' }}>Expected</td>
+            {expected.map((item, i) => (
+              <td key={i} style={{ padding: '0 8px' }}>{item}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/helpers/parseUtils.tsx
@@ -0,0 +1,74 @@
+import { CVS_COLUMNS, LEAF_NODE_ENDING, OUTLINE_LEVEL_INDEX, ERROR_TYPE_WRONG_HEADER, ERROR_TYPE_WRONG_OUTLINE } from './constants'
+import { CsvRow, TableRow } from "../types/data";
+import { validateOutlineLevels } from './validators'
+
+function parseValues(row: string[]): CsvRow {
+  const obj = {} as CsvRow;
+  row.forEach((field, index) => {
+    const key = CVS_COLUMNS[index].field as keyof CsvRow;
+    obj[key] = field;
+  });
+   
+  return obj;
+}
+
+export function validateExcelFile(data: string[][]): Record<string, any> {
+  const header = data[0];
+
+  const expectedHeaders = CVS_COLUMNS.map(c => c.header);
+  const headerMatch = header.length === expectedHeaders.length &&
+    header.every((value, index) => value === expectedHeaders[index]);
+
+  if (!headerMatch) {
+    return {
+      "error": ERROR_TYPE_WRONG_HEADER,
+      "expected_header": expectedHeaders,
+      "wrong_header": header,
+    }
+  }
+
+  const body = data.slice(1);
+  const errors = validateOutlineLevels(body, OUTLINE_LEVEL_INDEX)
+ 
+  if (errors.length > 0) {
+     return {
+      "error": ERROR_TYPE_WRONG_OUTLINE,
+      "errors": errors,
+    }
+  }
+
+  return {};
+}
+
+export function parseExcelFile(data: string[][]): TableRow[] {
+  const body = data.slice(1);
+  const root: TableRow[] = [];
+  const lookup: Record<string, TableRow> = {};
+
+  let keyCounter = 0;
+
+  for (const row of body) {
+    const id = row[0] as string;
+    const isLeaf = id.endsWith(LEAF_NODE_ENDING);
+    const parts = id.split(".");
+    const isRoot = parts.length === 1;
+    const parentKey = parts.slice(0, -1).join(".");
+
+    const node: TableRow = {id, key:keyCounter, data: parseValues(row), isLeaf: isLeaf, children: []};
+    lookup[id] = node;
+
+    if (isRoot) {
+        root.push(node);
+        continue;
+    }
+    const parent = lookup[parentKey];
+    // If parent does not exists skip, it is currently assumed that data is correctly sorted
+    if (!parent) {
+      continue;
+    }
+    lookup[parentKey].children.push(node);
+
+    keyCounter++;
+  }
+  return root;
+}
